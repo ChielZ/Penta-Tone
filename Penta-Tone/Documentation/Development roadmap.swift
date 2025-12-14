04b0d0088a633ec0cbb29980190d609b884787b2
Chiel Zwinkels <chiel.zwinkels@gmail.com>
2025-12-14 18:29:53 +0100

    Organised project files and folders

---
+++ Penta-Tone/Documentation/Development roadmap.swift
@@ -0,0 +1,19 @@
+//
+//  Development roadmap.swift
+//  Penta-Tone
+//
+//  Created by Chiel Zwinkels on 14/12/2025.
+//
+
+/*
+
+UI
+ - Implement scale type graphics display (raw shapes or image files?)
+ - Implement note name display
+ - Implement basic tooltip structure (toggle on/of in voice menu?)
+ 
+MINOR IMPROVEMENTS
+ - Change intonation display from ET/JI to EQUAL / JUST
+ - Check font warning
+ 
+ */
